a41239e0e2a725ac2e8ff677d805d021300158ba
dianachen1013 <157656188+dianachen1013@users.noreply.github.com>
2024-10-27 01:03:22 +0800

    ver2

---
+++ src/pages/api/scatterPlot.js
@@ -0,0 +1,19 @@
+import Points from './points';  // Import Points
+import XAxis from './xAxis';    // Import XAxis
+import YAxis from './yAxis';    // Import YAxis
+
+function ScatterPlot(props){
+    const { offsetX, offsetY, data, xScale, yScale, height, width } = props;
+    //task1: transform the <g> with the offsets so that the barchart can show properly 
+    //task2: import the components needed and uncomment the components in the return 
+    return (
+        <g transform={`translate(${offsetX}, ${offsetY})`}>
+            <Points data={data} xScale={xScale} yScale={yScale} height={height} width={width} />
+            <YAxis yScale={yScale} height={height} axisLabel={"Trip duration end in"} />
+            <XAxis xScale={xScale} height={height} width={width} axisLabel={"Trip duration start from"} />
+        </g>
+    );
+}
+
+export default ScatterPlot
+
